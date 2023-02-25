c25ea9c39f0e4b1170af38a6b57d14f27d9de27c
MOHAMEDAL ALBEDKHANI <102362263+mbrmj875@users.noreply.github.com>
2023-02-25 14:10:42 +0300

    Add files via upload

---
+++ laba4/Lab2_OOP/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Lab4_OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cmd = new System.Windows.Forms.TextBox();
+            this.log = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 531);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cmd
+            // 
+            this.cmd.BackColor = System.Drawing.SystemColors.ControlText;
+            this.cmd.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.cmd.Location = new System.Drawing.Point(63, 550);
+            this.cmd.Multiline = true;
+            this.cmd.Name = "cmd";
+            this.cmd.Size = new System.Drawing.Size(430, 28);
+            this.cmd.TabIndex = 1;
+            this.cmd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cmd_KeyDown);
+            // 
+            // log
+            // 
+            this.log.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.log.FormattingEnabled = true;
+            this.log.Location = new System.Drawing.Point(572, 551);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(438, 25);
+            this.log.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(508, 554);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Logging:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 554);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "CMD:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 588);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.cmd);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox cmd;
+        private System.Windows.Forms.ComboBox log;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ laba4/MyFigures/Figures/draw.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFigures
+{
+    public class draw : Figure
+    {
+        private Rectangle Body { get; set; }
+
+        static private int count = 0;
+
+        public draw() { }
+        public draw(int x, int y, int width, int height)
+        {
+            if (x < 0 || y < 0 || x + width > pictureBox.Width || y + height > pictureBox.Height)
+            {
+                MessageBox.Show("Фигура должна полностью помещаться на холст!");
+            }
+            else
+            {
+                this.x = x; this.y = y; this.width = width; this.height = width;
+                Create_all();
+                FList.RobotsList.Add(this);
+                FList.figureList.Add(this);
+                number = count;
+                count++;
+            }
+        }
+        public draw(string name, int x, int y, int width)
+        {
+            if (x < 0 || y < 0 || x + width > pictureBox.Width || y + height > pictureBox.Height)
+            {
+                MessageBox.Show("Фигура должна полностью помещаться на холст!");
+            }
+            else
+            {
+                this.x = x; this.y = y; this.width = width; this.height = width; this.name = name;
+                Create_all();
+                FList.RobotsList.Add(this);
+                FList.figureList.Add(this);
+                number = count;
+                count++;
+            }
+        }
+
+        private void Create_body()
+        {
+            Body = new Rectangle(x + width * 2 / 7, y + height * 3 / 7, width * 3 / 7, height * 3 / 7, false);
+        }
+
+
+
+        private void Create_all()
+        {
+            Create_body();
+
+        }
+        public override void Draw()
+        {
+            Body.Draw(false);
+
+            DrawText("Robot ", number, x + width * 2 / 7, y + height * 3 / 10, width * 3 / 7, height * 1 / 2);
+        }
+        public void Draw_with_name()
+        {
+            Body.Draw(false);
+
+            DrawText(name, x + width * 2 / 7, y + height * 3 / 10, width * 3 / 7, height * 1 / 2);
+        }
+        public override void MoveTo(int dx, int dy)
+        {
+            if (dx < 0 || dy < 0 || dx + width > pictureBox.Width || dy + height > pictureBox.Height)
+            {
+                MessageBox.Show("Фигура должна полностью помещаться на холст!");
+            }
+            else
+            {
+                x = dx; y = dy;
+                Create_all();
+                DeleteF(this, false);
+                Draw_with_name();
+            }
+        }
+
+
+        public void change_all(int x, int y)
+        {
+            if (x < 0 || y < 0 || x + width > pictureBox.Width || y + height > pictureBox.Height)
+            {
+                MessageBox.Show("Фигура должна полностью помещаться на холст!");
+            }
+            else
+            {
+                this.x = x; this.y = y;
+
+                Create_all();
+                DeleteF(this, false);
+                Draw_with_name();
+            }
+        }
+        public void DeleteF(draw figure, bool flag)
+        {
+            Graphics g = Graphics.FromImage(bitmap);
+            g.FillRectangle(Brushes.White, x, y, width, height);
+            FList.figureList.Remove(figure);
+            ClearMap();
+            foreach (draw f in FList.figureList)
+            {
+                f.Draw_with_name();
+            }
+            if (flag != true)
+            {
+                FList.figureList.Add(figure);
+            }
+            pictureBox.Image = bitmap;
+        }
+    }
+}
