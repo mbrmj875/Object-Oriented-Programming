78d629d65f341571b68728937872fcc5ac04fb33
MOHAMEDAL ALBEDKHANI <102362263+mbrmj875@users.noreply.github.com>
2023-02-25 15:05:50 +0300

    Add files via upload

---
+++ laba4/MyFigures/Operators/OperatorMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFigures.Operators
+{
+    public class OperatorMethod
+    {
+        public delegate void EmptyOperatorMethod();
+        public delegate void UnaryOperatorMethod(Object operand);
+        public delegate void BinaryOperatorMethod(Object operand1, Object operand2);
+    }
+}
